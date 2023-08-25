ca8f7ede6c1895dc8c59b0ec245867a8a371b754
Melanie Richards <mr@melanie-richards.com>
2023-08-24 21:18:11 -0700

    Disallow GPTBot, OpenAI's web crawler

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,10 @@ const shuffle = require('lodash.shuffle');
 
 module.exports = function(eleventyConfig) {
 
-  // Don't try to build asset files, just transparently copy them through
+  // Don't try to build pages from these files
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("README.md");
+  eleventyConfig.addPassthroughCopy("robots.txt");
 
   // Add shuffle filter to randomize arrays
   eleventyConfig.addFilter('shuffle', function(arr) {
